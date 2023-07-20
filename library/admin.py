09e27e1d8b4c2b412849a625315c4258fee35a2a
Serhii Mandziuk <s.f.mandziuk@gmail.com>
2023-07-20 22:58:37 +0300

    Updated admin module

--- library/admin.py
+++ library/admin.py
@@ -5,3 +5,66 @@ from . import models
 @admin.register(models.News)
 class NewsAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(models.Genre)
+admin.site.register(models.Language)
+
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = models.Book
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
+    model = models.BookInstance
+
+
+class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(models.Book, BookAdmin)
+
+
+@admin.register(models.BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
